bd15f2df63ef86535c263bfae565eab994c70807
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-04-30 17:08:03 +0100

    Add postgres db

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wzk!c0zbgy=uejm-jcfnmprsbqz_5d@0%8u5+%54jhv4b*)g+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
