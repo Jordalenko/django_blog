08f77033a25413f6bd1694e5eaa77da8e02d98a2
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-04 14:24:49 +0100

    Add comments model

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     STATUS = ((0, "Draft"), (1, "Published"))
 
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    STATUS = ((0, "Draft"), (1, "Published"))
