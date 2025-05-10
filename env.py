54c0c58870f1d552c646d84f179e4f59fb56d0c0
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-10 14:02:31 +0100

    Add secret key to env.py

--- env.py
+++ env.py
@@ -2,3 +2,7 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_xh8EbyD5Iejo@ep-shy-frog-a25zaifs.eu-central-1.aws.neon.tech/wish_opal_army_593047")
+
+os.environ.setdefault(
+    "SECRET_KEY", 'django-insecure-wzk!c0zbgy=uejm-jcfnmprsbqz_5d@0%8u5+%54jhv4b*)g+!'
+)
