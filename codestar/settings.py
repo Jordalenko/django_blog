402ce82b2ac8dcda145569b3b0d744434ea251b7
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-10 14:07:34 +0100

    Save changes made to settings file for envirinment secret key to env.py file.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wzk!c0zbgy=uejm-jcfnmprsbqz_5d@0%8u5+%54jhv4b*)g+!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
