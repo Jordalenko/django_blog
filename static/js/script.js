4f7927ea3f4f01b410bf24c7b1732c571fafb84c
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-22 18:33:49 +0100

    Add Comment edit and delete functionality.

--- static/js/script.js
+++
@@ -1,4 +0,0 @@
-console.log("Life, The Universe and Everything!");
-// This is a simple JavaScript file that logs "Hello, World!" to the console.
-// You can add more functionality here as needed.
-// For example, you can create a function that adds two numbers:
