761222410a9e6c2d5f266a681cf2d160d23c7b89
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-24 01:25:27 +0100

    add cloudinary image services to about page

--- about/models.py
+++ about/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
