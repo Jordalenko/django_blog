065b21abfb487337d6e9430d55428310b60e2667
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-25 13:39:29 +0100

    add about tests

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -6,8 +6,8 @@ class TestCommentForm(TestCase):
 
     def test_form_is_valid(self):
         comment_form = CommentForm({'body': 'This is a test comment.'})
-        self.assertTrue(comment_form.is_valid())
+        self.assertTrue(comment_form.is_valid(), msg="Form is not valid")
 
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
-        self.assertFalse(comment_form.is_valid())
+        self.assertFalse(comment_form.is_valid(), msg="Form is valid when it shouldn't be")
