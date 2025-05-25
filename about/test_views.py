ae8c64e992c5934723ce5661ab0ac1b52b9c5f93
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-25 22:14:34 +0100

    add views tests for POST methods

--- about/test_views.py
+++ about/test_views.py
@@ -20,3 +20,18 @@ class TestAboutViews(TestCase):
         self.assertIn(b"about me", response.content)
         self.assertIsInstance(
             response.context['collaborate_form'], CollaborateForm)
+        
+    def test_successful_collaboration_request_submission(self):
+        """Test for a user requesting a collaboration"""
+        post_data = {
+            'name': 'test name',
+            'email': 'test@email.com',
+            'message': 'test message'
+        }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            (b'Collaboration request received! I endeavour to respond within 2 '
+             b'working days.'),
+            response.content
+        )
